bde0e540b431c4b05c938ce49a8b56de79a41911
Teresa De Jesus <teresa1594@gmail.com>
2018-11-18 21:29:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,6 @@ end
 
 def turn (board)
   puts "Please enter 1-9:"
-user_input=gets.strip
 end
 
 def input_to_index(input)
@@ -21,18 +20,42 @@ def input_to_index(input)
   return index
 end
 
-def move(board, index, user_marker = "X")
-  board[index.to_i] = user_marker
-end
 def valid_move?(board, index)
-  if position_taken?(board,index) == false && (index).between(0,8) == true
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
-  else false
+  else
+    return false
   end
 end
-def position_taken? (board, index)
-  if board [index] == " " || board [index] == "" || board[index] == nil
-    return false
-  else return true
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+def turn (board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
   end
 end
