94b560d2da338416a881deb8485019b6c3ba723b
Teresa De Jesus <teresa1594@gmail.com>
2018-11-18 21:49:01 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,7 +18,12 @@ def valid_move?(board, index)
   else false
   end
 end
-
+def position_taken?(board, index)) == false && (on_board?(index) == true)
+  return true
+else
+  return false
+end
+end
   def on_board?(num)
     if num.between?(0, 8) == true
       return true
@@ -27,12 +32,7 @@ end
     end
   end
 
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
-end
+
 
 def move(board, index, character = "X")
   board[index] = character
