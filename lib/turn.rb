c34117dae30b2c93993f8b868e9c1cc7ec06e662
Teresa De Jesus <teresa1594@gmail.com>
2018-11-18 21:47:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,13 +13,11 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
+  if position_taken?(board,index)==false && (index).between(0,8)==true
+    return true
+  else false
+  end 
+end
 
   def on_board?(num)
     if num.between?(0, 8) == true
